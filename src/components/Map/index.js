b016893fc8221da687bff89447fb300e0579b375
Benjamin Shover <benjamin.shover@allisontransmission.com>
2026-04-27 18:17:59 -0400

    render map on browser only

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-require('leaflet/dist/leaflet.css');
+import 'leaflet/dist/leaflet.css';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const troopIcon = new L.Icon ({
     iconUrl: '/img/map-marker/troop-marker.png',
@@ -21,18 +22,20 @@ const cubIcon = new L.Icon ({
 
 function Map() {
   return (
-    <MapContainer center={[39.83398, -86.38716]} zoom={14} style={{ height: '400px', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={[39.84048, -86.387]} icon={troopIcon}>
-        <Popup>American Legion Post 331<br />Troop 303<br />Troop 331<br />Crew 303</Popup>
-      </Marker>
-      <Marker position={[39.82795329321996, -86.38975014166927]} icon={cubIcon}>
-        <Popup>Eagle Elementary School<br />Pack 303</Popup>
-      </Marker>
-    </MapContainer>
+    <BrowserOnly>
+      <MapContainer center={[39.83398, -86.38716]} zoom={14} style={{ height: '400px', width: '100%' }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[39.84048, -86.387]} icon={troopIcon}>
+          <Popup>American Legion Post 331<br />Troop 303<br />Troop 331<br />Crew 303</Popup>
+        </Marker>
+        <Marker position={[39.82795329321996, -86.38975014166927]} icon={cubIcon}>
+          <Popup>Eagle Elementary School<br />Pack 303</Popup>
+        </Marker>
+      </MapContainer>
+    </BrowserOnly>
   );
 }
 export default Map;
