edfc93dfb01d83be1cd248825c9c7f41e934c0e1
shoverbj <shoverbj@gmail.com>
2026-04-27 16:43:54 -0400

    retry

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import '~leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';
 
 const troopIcon = new L.Icon ({
     iconUrl: '/img/map-marker/troop-marker.png',
