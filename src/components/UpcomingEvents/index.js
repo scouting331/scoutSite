15c9b6888bf3b034a09a911a49fee798a72ca1ad
Benjamin Shover <benjamin.shover@allisontransmission.com>
2026-04-29 21:58:13 -0400

    so many changes

--- src/components/UpcomingEvents/index.js
+++ src/components/UpcomingEvents/index.js
@@ -1,15 +1,18 @@
+import Heading from '@theme/Heading';
 
 export default function UpcomingEvents({}) {
     return (
         <div>
-            <h1 className="text--center">
+            <Heading as='h1' className="text--center">
                 Upcoming Events
-            </h1>
-            <iframe id="open-web-calendar" 
-                src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?css=.CALENDAR-INDEX-0%2C%20.CALENDAR-INDEX-0%20.dhx_body%2C%20.CALENDAR-INDEX-0%20.dhx_title%20%20%7B%20background-color%3A%20%23fcd116%3B%20%7D%20%0A.CALENDAR-INDEX-1%2C%20.CALENDAR-INDEX-1%20.dhx_body%2C%20.CALENDAR-INDEX-1%20.dhx_title%20%20%7B%20background-color%3A%20%23d6cebd%3B%20%7D%20%0A&amp;event_url_geo=https%3A%2F%2Fwww.google.com%2Fmaps%2F%40%7Blat%7D%2C%7Blon%7D%2C%7Bzoom%7Dz&amp;event_url_location=https%3A%2F%2Fwww.google.com%2Fmaps%2Fsearch%2F%7Blocation%7D&amp;hour_format=%25g%3A%25i%E2%80%AF%25a&amp;loader=&amp;menu_shows_title=false&amp;skin=flat&amp;start_of_week=su&amp;tabs=month&amp;target=_blank&amp;timezone=America%2FNew_York&amp;title=Upcoming%20Events&amp;url=https%3A%2F%2Fapi.scouting.org%2Fadvancements%2Fevents%2Fcalendar%2F51646&amp;url=https%3A%2F%2Fapi.scouting.org%2Fadvancements%2Fevents%2Fcalendar%2F48074"
-                sandbox="allow-scripts allow-same-origin allow-popups allow-downloads allow-popups-to-escape-sandbox"
-                allowTransparency="true" scrolling="no" 
-                frameborder="0" height="552px" width="70%"></iframe>
+            </Heading>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&showTitle=0&src=cmdkYmNwYWcwazg0b3FtbWs3bWF0bTRmYzUwdW4zcmVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZDR2NHFiaXE3anYxZDQxOWRhaGh2dWJhNGVxZDhya3BAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%234285f4&color=%23009688" 
+                style={{border:0}}
+                width="80%" 
+                height="600" 
+                frameborder="0" 
+                scrolling="no">
+            </iframe>
         </div>
     );
 }
--- src/pages/join-us.js
+++ src/pages/join-us.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import MapWrapper from '@site/src/components/Map'
+import Link from '@docusaurus/Link';
+import Heading from '@theme/Heading';
 
 export default function JoinUs() {
   return (
     <Layout title="Join Us" description='Information on joining our organization'>
         <div style={{ padding: '2rem', maxWidth: '1200px', margin: '0 auto', fontFamily: 'sans-serif' }}>
         <header>
-            <h1>Join Us</h1>
+            <Heading as='h1'>Join Us</Heading>
             <p>Join Our Units</p>
         </header>
 
         <section>
-            <h2>Join Our Troop</h2>
+            <Heading as='h2'>Join Our Troop</Heading>
             <ul>
                 <li>
-                    Join <a href="https://beascout.scouting.org/list/?gad_source=1&gad_campaignid=23532089962&zip=46112&program%5B%5D=scoutsBSA&unitID=84066" target="_blank" rel="noopener noreferrer">Troop 303</a>, our boys troop
+                    Join <Link to="https://beascout.scouting.org/list/?gad_source=1&gad_campaignid=23532089962&zip=46112&program%5B%5D=scoutsBSA&unitID=84066">Troop 303</Link>, our boys troop
                 </li>
                 <li>
-                    Join <a href="https://beascout.scouting.org/list/?gad_source=1&gad_campaignid=23532089962&zip=46112&program%5B%5D=scoutsBSA&unitID=358986" target="_blank" rel="noopener noreferrer">Troop 331</a>, our girls troop
+                    Join <Link to="https://beascout.scouting.org/list/?gad_source=1&gad_campaignid=23532089962&zip=46112&program%5B%5D=scoutsBSA&unitID=358986">Troop 331</Link>, our girls troop
                 </li>
                 <li>
-                    Join <a href="https://beascout.scouting.org/list/?zip=46112&program[]=pack&miles=20&unitID=284097" target="_blank" rel="noopener noreferrer">Pack 303</a>, our group for elementary students grades K-5th.
+                    Join <Link to="https://beascout.scouting.org/list/?zip=46112&program[]=pack&miles=20&unitID=284097">Pack 303</Link>, our group for elementary students grades K-5th.
                 </li>
             </ul>
         </section>
 
         <section style={{ marginTop: '2rem' }}>
-            <h2>Weekly Meetings</h2>
+            <Heading as='h2'>Weekly Meetings</Heading>
             <p>
             Our weekly meetings are where the youth work on advancement, merit badges,
             communicate upcoming plans and events, and build friendships with their fellow
@@ -36,17 +38,17 @@ export default function JoinUs() {
 
             <div style={{ display: 'flex', flexWrap: 'wrap', gap: '2rem', marginTop: '1.5rem' }}>
                 <div style={{ flex: '1', minWidth: '300px' }}>
-                    <h3>Meeting Days</h3>
+                    <Heading as='h3'>Meeting Days</Heading>
                     <p>Every Tuesday (except school holidays)</p>
 
-                    <h3>Meeting Time</h3>
+                    <Heading as='h3'>Meeting Time</Heading>
                     <p>6:30pm - 8:00pm</p>
 
-                    <h3>Location</h3>
+                    <Heading as='h3'>Location</Heading>
                     <address style={{ fontStyle: 'normal' }}>
-                    American Legion Post 331<br />
-                    636 E Main St<br />
-                    Brownsburg, IN 46112
+                        American Legion Post 331<br />
+                        636 E Main St<br />
+                        Brownsburg, IN 46112
                     </address>
                 </div>
                 <div style={{ flex: '1', minWidth: '300px' }}>
