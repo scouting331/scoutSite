8010256df09b16e7f2173c30c6d684b4363fa391
Benjamin Shover <shoverbj@gmail.com>
2026-05-21 13:05:02 -0400

    Update index.js

--- src/components/UpcomingEvents/index.js
+++ src/components/UpcomingEvents/index.js
@@ -7,11 +7,12 @@ export default function UpcomingEvents({}) {
         Upcoming Events
       </Heading>
       <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FIndiana%2FIndianapolis&showPrint=0&showTabs=0&showTz=0&src=ZDR2NHFiaXE3anYxZDQxOWRhaGh2dWJhNGVxZDhya3BAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=cmdkYmNwYWcwazg0b3FtbWs3bWF0bTRmYzUwdW4zcmVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23009688&color=%234285f4"
-        style="border-width:0" 
-        width="800" 
+        style={{ border: 0 }} 
+        width="80%" 
         height="600" 
         frameborder="0" 
-        scrolling="no"></iframe>
+        scrolling="no"
+      ></iframe>
     </div>
   );
 }
