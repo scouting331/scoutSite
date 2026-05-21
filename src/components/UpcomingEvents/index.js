362e06c90dc471566b4cd47eab337c12f93c0df1
Benjamin Shover <shoverbj@gmail.com>
2026-05-21 12:27:51 -0400

    Update index.js

--- src/components/UpcomingEvents/index.js
+++ src/components/UpcomingEvents/index.js
@@ -6,14 +6,12 @@ export default function UpcomingEvents({}) {
       <Heading as="h1" className="text--center">
         Upcoming Events
       </Heading>
-      <iframe
-        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&showTitle=0&src=cmdkYmNwYWcwazg0b3FtbWs3bWF0bTRmYzUwdW4zcmVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZDR2NHFiaXE3anYxZDQxOWRhaGh2dWJhNGVxZDhya3BAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%234285f4&color=%23009688"
-        style={{ border: 0 }}
-        width="80%"
-        height="600"
-        frameBorder="0"
-        scrolling="no"
-      ></iframe>
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FIndiana%2FIndianapolis&showPrint=0&showTabs=0&showTz=0&src=ZDR2NHFiaXE3anYxZDQxOWRhaGh2dWJhNGVxZDhya3BAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&src=cmdkYmNwYWcwazg0b3FtbWs3bWF0bTRmYzUwdW4zcmVAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23009688&color=%234285f4"
+        style="border-width:0" 
+        width="800" 
+        height="600" 
+        frameborder="0" 
+        scrolling="no"></iframe>
     </div>
   );
 }
